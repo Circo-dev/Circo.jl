795c66e61cce1e6a286e93e463c27346ad99871b
Schaffer Krisztian <schaffer.krisztian@gmail.com>
2020-01-03 15:32:39 +0100

    Fixed format tests, removed perf test

--- test/formats/dataframe.jl
+++ test/formats/dataframe.jl
@@ -1,8 +1,9 @@
 using Test, Circo, CSV
 
 @testset "DataFrames" begin
-    workflow = CSV.read("test/assets/onecolumn.csv") | (row -> 2 * row.col1)
+    filename = joinpath(@__DIR__, "onecolumn.csv")
+    workflow = CSV.read(filename) | (row -> 2 * row.col1)
     @test workflow() == 88
-    biggerflow = CSV.read("test/assets/onecolumn.csv") | ((row -> 2 * row.col1) | (x->2x))
+    biggerflow = CSV.read(filename) | ((row -> 2 * row.col1) | (x->2x))
     @test biggerflow() == 176
 end
