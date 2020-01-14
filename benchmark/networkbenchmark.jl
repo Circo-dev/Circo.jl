60563e259d32baaec60672f508a4de24aec86b28
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2020-01-14 15:26:58 +0100

    Benchmark of a 1000 nodes chain

--- benchmark/networkbenchmark.jl
+++ benchmark/networkbenchmark.jl
@@ -1,6 +1,14 @@
 using Circo
 using BenchmarkTools
 
+function createchain(chainlength=1000)
+    chain = Network([])
+    for i=1:chainlength
+        chain | (x->x+1)
+    end
+    return chain
+end
+
 function iterate_network!(network, itercount)
     sum = 0
     for stepnum in 1:itercount
@@ -17,3 +25,4 @@ SUITE["Network"] = BenchmarkGroup()
 SUITE["Network"]["1node_1000steps2x"] = @benchmarkable iterate_network!(network, 1000) setup=(network = Network([Node(x -> 2x)]))
 SUITE["Network"]["2nodes_1000steps2xplus1"] = @benchmarkable iterate_network!(network, 1000) setup=(network = ((x -> 2x) | (x -> x+1)))
 SUITE["Network"]["4nodes_1000steps4xplus2"] = @benchmarkable iterate_network!(network, 1000) setup=(network = ((x -> 2x) | (x -> 2x) | (x -> x+1) | (x -> x+1)))
+SUITE["Network"]["1000nodes_1step"] = @benchmarkable iterate_network!(network, 1) setup=(network = (createchain()))
