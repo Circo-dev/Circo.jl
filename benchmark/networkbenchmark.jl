a324c761031d17df0fbf51dd12cebe181336ed68
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2020-01-22 11:40:37 +0100

    Fixed network benchmarks

--- benchmark/networkbenchmark.jl
+++ benchmark/networkbenchmark.jl
@@ -9,10 +9,10 @@ function createchain(chainlength=1000)
     return chain
 end
 
-function iterate_network!(network, itercount)
+function iterate_network!(scheduler, itercount)
     sum = 0
     for stepnum in 1:itercount
-        sum += network(stepnum)
+        sum += scheduler(stepnum)
     end
     return sum
 end
@@ -22,7 +22,7 @@ end
 #@btime iterate_network!(network, 1000) setup=(network = ((x -> 2x) | (x -> x+1)))
 
 SUITE["Network"] = BenchmarkGroup()
-SUITE["Network"]["1node_1000steps2x"] = @benchmarkable iterate_network!(network, 1000) setup=(network = Network([Node(x -> 2x)]))
-SUITE["Network"]["2nodes_1000steps2xplus1"] = @benchmarkable iterate_network!(network, 1000) setup=(network = ((x -> 2x) | (x -> x+1)))
-SUITE["Network"]["4nodes_1000steps4xplus2"] = @benchmarkable iterate_network!(network, 1000) setup=(network = ((x -> 2x) | (x -> 2x) | (x -> x+1) | (x -> x+1)))
-SUITE["Network"]["1000nodes_1step"] = @benchmarkable iterate_network!(network, 1) setup=(network = (createchain()))
+SUITE["Network"]["1node_1000steps2x"] = @benchmarkable iterate_network!(scheduler, 1000) setup=(scheduler = SimpleScheduler(Network([Node(x -> 2x)])))
+SUITE["Network"]["2nodes_1000steps2xplus1"] = @benchmarkable iterate_network!(scheduler, 1000) setup=(scheduler = SimpleScheduler(((x -> 2x) | (x -> x+1))))
+SUITE["Network"]["4nodes_1000steps4xplus2"] = @benchmarkable iterate_network!(scheduler, 1000) setup=(scheduler = SimpleScheduler(((x -> 2x) | (x -> 2x) | (x -> x+1) | (x -> x+1))))
+SUITE["Network"]["1000nodes_1step"] = @benchmarkable iterate_network!(scheduler, 1) setup=(scheduler = (SimpleScheduler(createchain())))
