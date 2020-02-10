6aac522c0974586132e58297f292780d0808a6ab
Schäffer Krisztián <schaffer.krisztian@gmail.com>
2020-02-10 13:11:40 +0100

    Actor memory measurement script added

---
+++ benchmark/memory/actor-memory-usage.jl
@@ -0,0 +1,32 @@
+# This simple script measures the memory consumption of actor instances by creating 1e6 of them
+# and calling varinfo()
+
+using Circo
+import Circo.onmessage
+
+machine = Machine()
+
+varinfo()
+
+actorids=Array{ComponentId}(undef, 0)
+
+mutable struct SimpleActor <: Component
+    id::ComponentId
+    SimpleActor() = new(rand(ComponentId))
+end
+
+function onmessage(service, me::SimpleActor, message::Message{String})
+    println("Actor #$(id(me)) got message $(body(message))")
+end
+
+for i in 1:1000000
+    a = SimpleActor()
+    push!(actorids, id(a))
+    spawn(machine, a)
+end
+
+varinfo()
+
+for i=1:10
+    machine(Message{String}(0, actorids[i], "Hello"))
+end
